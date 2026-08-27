142e350ee61477d2220486daf477b9cc2c78a006
Rick Brown <rick.brown@gmail.com>
2026-08-27 18:51:56 -0400

    REHLDS_INSTALL variable to control ReHLDS install

--- container/rehlds_build.sh
+++ container/rehlds_build.sh
@@ -5,7 +5,9 @@ set -o nounset
 
 export GAME="valve-legacy"
 export FLAG="-beta steam_legacy"
+export REHLDS_INSTALL=true
 
 cd "$(dirname "$0")"
 
+# Pass any additional cmdline opts to build process
 docker compose build "$@"
